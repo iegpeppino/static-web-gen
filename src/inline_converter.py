35538f645258361a1fd250a53b5308720fcd89f2
iegpeppino <iegpeppino@outlook.com.ar>
2025-04-07 19:14:20 +0000

    E: create split_node_images , split_node_links to inline convertesrs and add unittests

--- src/inline_converter.py
+++ src/inline_converter.py
@@ -1,5 +1,5 @@
 from textnode import TextNode, TextType
-
+import re
 
 """
 Takes a list of old text nodes, a delimiter and text type
@@ -14,15 +14,68 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
             new_nodes.append(node)
             continue
         split_nodes = []
-        splits = node.text.split(delimiter)
-        if len(splits) % 2 == 0:  # Checking if there's a closing delimiter
+        sections = node.text.split(delimiter)
+        if len(sections) % 2 == 0:  # Checking if there's a closing delimiter
             raise ValueError("invalid markdown, formatted section not closed")
-        for i in range(len(splits)):
-            if splits[i] == "":
+        for i in range(len(sections)):
+            if sections[i] == "":
                 continue
             if i % 2 == 0:
-                split_nodes.append(TextNode(splits[i], TextType.TEXT))
+                split_nodes.append(TextNode(sections[i], TextType.TEXT))
             else:
-                split_nodes.append(TextNode(splits[i], text_type))
+                split_nodes.append(TextNode(sections[i], text_type))
         new_nodes.extend(split_nodes)
     return new_nodes
+
+def extract_markdown_images(text):
+    #tuple = re.findall(r"\!\[(\w+)\]\((.*?)\)",text)
+    return re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+    
+
+def extract_markdown_links(text):
+    return re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+
+def split_nodes_images(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+        images = extract_markdown_images(node.text)        
+        if len(images) == 0:
+            new_nodes.append(node) # If the node has no img or link item append directly
+        for image in images:
+            sections = node.text.split(f"![{image[0]}]({image[1]})", 1) # image[0] is alt_text and image[1] is img_url
+            if len(sections) != 2:
+                raise ValueError("invalid markdown, image section not closed")
+            if sections[0] != "":
+                new_nodes.append(TextNode(sections[0], TextType.TEXT))  # If previous section to img has text, append as textype.text node
+            new_nodes.append(TextNode(image[0], TextType.IMG, image[1]))
+            original_text = sections[1]
+        if original_text != "":
+            new_nodes.append(TextNode(original_text, TextType.TEXT))
+    return new_nodes
+
+def split_nodes_links(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+        links = extract_markdown_links(node.text)
+        if len(links) == 0:
+            new_nodes.append(node)
+            continue
+        for link in links:
+            sections = node.text.split(f"[{link[0]}]({link[1]})", 1)
+            if len(sections) != 2:
+                raise ValueError("invalid markdown, link section not closed")
+            if sections[0] != "":
+                new_nodes.append(TextNode(sections[0], TextType.TEXT))
+            new_nodes.append(TextNode(link[0], TextType.LINK, link[1]))
+            original_text = sections[1]
+        if original_text != "":
+            new_nodes.append(TextNode(original_text, TextType.TEXT))
+    return new_nodes
+
+
--- src/test_inline_converter.py
+++ src/test_inline_converter.py
@@ -1,7 +1,7 @@
 import unittest
 
 from textnode import TextNode, TextType, text_node_to_html_node
-from inline_converter import split_nodes_delimiter
+from inline_converter import split_nodes_delimiter, extract_markdown_images, extract_markdown_links, split_nodes_images
 
 class TestInlineConverter(unittest.TestCase):
 
@@ -76,6 +76,62 @@ class TestInlineConverter(unittest.TestCase):
             ],
             new_nodes,
         )
-        
+    
+    # TESTING MARKDOWN IMAGE AND LINK EXTRACTORS 
+
+    def test_ext_markdown_img(self):
+        text = "![alt_text](https://www.google.com)"
+        self.assertEqual(extract_markdown_images(text), [("alt_text", "https://www.google.com")])
+    
+    def test_ext_full_sentence_markdown_img(self):
+        text = "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        self.assertEqual(extract_markdown_images(text), [("image", "https://i.imgur.com/zjjcJKZ.png")])
+
+    # Multiple image links in text
+    def test_ext_multiple_links(self):
+        text = "Cute puppy img : ![doggy](https://cutedogpic.com) and cool guitar img : ![guitar](https://coolguitar.com)"
+        self.assertEqual(extract_markdown_images(text), [("doggy", "https://cutedogpic.com"), ("guitar", "https://coolguitar.com")])
+
+    # Input is a regular link and not an img link
+    def test_ext_mkdown_imgurl_from_link(self):
+        text = "[to google](https://www.google.com)"
+        self.assertNotEqual(extract_markdown_images(text), [("to google", "https://www.google.com")])
+
+    # Both a image link and regular link are present in the text
+    def test_ext_mkdown_img_diff_links(self):
+        text = "link to web [webpage](https://12lkasmfa.com) and link to img ![image](https://i.imgur.com/zjjcJKZ.png)"
+        self.assertEqual(extract_markdown_images(text), [("image", "https://i.imgur.com/zjjcJKZ.png")])
+
+    def test_ext_markdown_link(self):
+        text = "[to google](https://www.google.com)"
+        self.assertEqual(extract_markdown_links(text), [("to google", "https://www.google.com")])
+    
+    def test_ext_full_sentence_markdown_link(self):
+        text = "This link points to google's webpage: [to google](https://www.google.com)"
+        self.assertEqual(extract_markdown_links(text), [("to google", "https://www.google.com")])
+    
+    # Text has multiple links
+    def test_ext_multiple_markdown_links(self):
+        text = "This link points to google [google](https://google.com) this other one poinst to nurgle [nurgle](https://www.nurgle.com)"
+        self.assertEqual(extract_markdown_links(text), [("google","https://google.com"), ("nurgle","https://www.nurgle.com")])
+
+    # Gives an img url instead of a regular link as input
+    def test_ext_mkdown_link_with_img(self):
+        text = "![alt_text](https://www.google.com)"
+        self.assertNotEqual(extract_markdown_links(text), [("to google", "https://www.google.com")])
+
+    # Both a image link and regular link are present in the text
+    def test_ext_mkdown_link_diff_links(self):
+        text = "link to web [webpage](https://12lkasmfa.com) and link to img ![image](https://i.imgur.com/zjjcJKZ.png)"
+        self.assertEqual(extract_markdown_links(text), [("webpage", "https://12lkasmfa.com")])
+
+    def test_split_node_img(self):
+        node = TextNode("This textnode contains an image ![image_alt](https://image.url)", TextType.TEXT)
+        self.assertEqual(
+            split_nodes_images([node]),
+            [TextNode("This textnode contains an image ", TextType.TEXT),
+            TextNode("image_alt", TextType.IMG, "https://image.url"),]
+            )
+
 if __name__ == "__main__":
     unittest.main()
